f793c6ecb521d94bef2d0eac44fa51650cbbfbb3
Samuel Gwilym <sam@comique.co>
2015-10-09 14:59:09 +0200

    Refactor scene image loading

--- src/SceneImages.js
+++
@@ -1,4 +0,0 @@
-module.exports = [
-  './scene-images/panel-a.png',
-  './scene-images/panel-b.png'
-];
---
+++ src/scene-images/index.js
@@ -0,0 +1,7 @@
+const sceneImageRequire = require.context('./', false, /.*\.(png)/);
+
+const sceneImagePaths = sceneImageRequire.keys();
+
+export default sceneImagePaths.map((path) => {
+  return require(path);
+});
